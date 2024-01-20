ef4911dac7ffa298b33bb4f01e39c176db873aed
username <wecp-dummy@gmail.com>
2024-01-20 06:08:30 +0000

    wecp initial commit

---
+++ backend/src/main/java/com/wecp/progressive/dao/AccountDAOImpl.java
@@ -0,0 +1,62 @@
+package com.wecp.progressive.dao;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.security.auth.login.AccountNotFoundException;
+
+import com.wecp.progressive.entity.Accounts;
+
+public class AccountDAOImpl implements AccountDAO{
+      private   List<Accounts> acc=new ArrayList<Accounts>();
+   
+    @Override
+    public int addAccount(Accounts accounts) {
+        // TODO Auto-generated method stub
+        acc.add(accounts);
+       return 1;
+    
+        //throw new UnsupportedOperationException("Unimplemented method 'addAccount'");
+    }
+
+    @Override
+    public Accounts getAccountById(int accountId) {
+        // TODO Auto-generated method stub
+        Accounts ac=null;
+        for(Accounts a:acc){
+            if(a.getAccount_id()==accountId){
+                ac= a;
+                
+            }
+            else{ try {
+                throw new AccountNotFoundException();
+            } catch (AccountNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }}
+        }
+        return ac;
+        
+        //throw new UnsupportedOperationException("Unimplemented method 'getAccountById'");
+    }
+
+    @Override
+    public void updateAccount(Accounts accounts) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateAccount'");
+    }
+
+    @Override
+    public void deleteAccount(int accountId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteAccount'");
+    }
+
+    @Override
+    public List<Accounts> getAllAccounts() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllAccounts'");
+    }
+
+}
