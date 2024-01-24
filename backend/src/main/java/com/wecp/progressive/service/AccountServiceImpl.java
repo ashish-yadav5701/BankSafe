3a4f68604112f4b0aed418a47b1380433952b6af
username <wecp-dummy@gmail.com>
2024-01-24 11:34:56 +0000

    third

--- backend/src/main/java/com/wecp/progressive/service/AccountServiceImpl.java
+++ backend/src/main/java/com/wecp/progressive/service/AccountServiceImpl.java
@@ -1,7 +1,98 @@
 package com.wecp.progressive.service;
 
+import java.sql.SQLException;
+
+import java.util.*;
+
+import com.wecp.progressive.dao.AccountDAO;
+import com.wecp.progressive.entity.Accounts;
 
 
-public class AccountServiceImpl  {
 
+public class AccountServiceImpl implements AccountService {
+    private static List<Accounts> al;
+    private AccountDAO accDao;
+    public AccountServiceImpl(AccountDAO accDao) {
+        this.accDao = accDao;
+    }
+
+    @Override
+    public List<Accounts> getAllAccounts() throws SQLException {
+        // TODO Auto-generated method stub
+        return al;
+        //throw new UnsupportedOperationException("Unimplemented method 'getAllAccounts'");
+    }
+
+    @Override
+    public List<Accounts> getAccountsByUser(int userId) throws SQLException {
+        // TODO Auto-generated method stub
+        return al;
+        //throw new UnsupportedOperationException("Unimplemented method 'getAccountsByUser'");
+    }
+
+    @Override
+    public Accounts getAccountById(int accountId) throws SQLException {
+        // TODO Auto-generated method stub
+        return null;
+        //throw new UnsupportedOperationException("Unimplemented method 'getAccountById'");
+    }
+
+    @Override
+    public int addAccount(Accounts accounts) throws SQLException {
+        // TODO Auto-generated method stub
+        al.add(accounts);
+        return 1;
+        //throw new UnsupportedOperationException("Unimplemented method 'addAccount'");
+    }
+
+    @Override
+    public void updateAccount(Accounts accounts) throws SQLException {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'updateAccount'");
+    }
+
+    @Override
+    public void deleteAccount(int accountId) throws SQLException {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'deleteAccount'");
+    }
+
+    @Override
+    public List<Accounts> getAllAccountsSortedByBalance() throws SQLException {
+        // TODO Auto-generated method stub
+       return null;
+       // throw new UnsupportedOperationException("Unimplemented method 'getAllAccountsSortedByBalance'");
+    }
+
+    @Override
+    public List<Accounts> getAllAccountsFromArrayList() {
+        // TODO Auto-generated method stub
+        return null;
+        //throw new UnsupportedOperationException("Unimplemented method 'getAllAccountsFromArrayList'");
+    }
+
+    @Override
+    public List<Accounts> addAccountToArrayList(Accounts accounts) {
+        // TODO Auto-generated method stub
+        al.add(accounts);
+        return al;
+        //throw new UnsupportedOperationException("Unimplemented method 'addAccountToArrayList'");
+    }
+
+    @Override
+    public List<Accounts> getAllAccountsSortedByBalanceFromArrayList() {
+        // TODO Auto-generated method stub
+        List<Accounts> sortedList=al;
+        Collections.sort(sortedList);
+        return sortedList;
+       // throw new UnsupportedOperationException("Unimplemented method 'getAllAccountsSortedByBalanceFromArrayList'");
+    }
+
+    @Override
+    public void emptyArrayList() {
+        // TODO Auto-generated method stub
+       al=new ArrayList<>();
+        //throw new UnsupportedOperationException("Unimplemented method 'emptyArrayList'");
+    }
+   
 }
--- backend/src/main/java/com/wecp/progressive/service/CustomerServiceImpl.java
+++ backend/src/main/java/com/wecp/progressive/service/CustomerServiceImpl.java
@@ -1,5 +1,80 @@
 package com.wecp.progressive.service;
 
-public class CustomerServiceImpl {
+import java.sql.SQLException;
+import java.util.*;
+
+import com.wecp.progressive.dao.CustomerDAO;
+import com.wecp.progressive.entity.Customers;
+
+public class CustomerServiceImpl implements CustomerService {
+     private List<Customers> cList=new ArrayList<>();
+    private CustomerDAO cDao;
+    
+    public CustomerServiceImpl(CustomerDAO cDao) {
+        this.cDao = cDao;
+    }
+
+    @Override
+    public List<Customers> getAllCustomers() throws SQLException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllCustomers'");
+    }
+
+    @Override
+    public Customers getCustomerById(int customerId) throws SQLException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCustomerById'");
+    }
+
+    @Override
+    public int addCustomer(Customers customers) throws SQLException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addCustomer'");
+    }
+
+    @Override
+    public void updateCustomer(Customers customers) throws SQLException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateCustomer'");
+    }
+
+    @Override
+    public void deleteCustomer(int customerId) throws SQLException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteCustomer'");
+    }
+
+    @Override
+    public List<Customers> getAllCustomersSortedByName() throws SQLException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllCustomersSortedByName'");
+    }
+
+    @Override
+    public List<Customers> getAllCustomersFromArrayList() {
+        // TODO Auto-generated method stub
+        return cList;
+        //throw new UnsupportedOperationException("Unimplemented method 'getAllCustomersFromArrayList'");
+    }
+
+    @Override
+    public List<Customers> addCustomersToArrayList(Customers customers) {
+        // TODO Auto-generated method stub
+        cList.add(customers);
+        return cList;
+        //throw new UnsupportedOperationException("Unimplemented method 'addCustomersToArrayList'");
+    }
+
+    @Override
+    public List<Customers> getAllCustomersSortedByNameFromArrayList() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllCustomersSortedByNameFromArrayList'");
+    }
+
+    @Override
+    public void emptyArrayList() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'emptyArrayList'");
+    }
 
 }
